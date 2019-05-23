3ea23b9506875cbf0ea78172052a81761576fe25
Blanca <aidil0308@outlook.com>
2019-05-22 20:14:24 -0600

    23

--- js/acciones.js
+++ js/acciones.js
@@ -1,26 +1,32 @@
-//JavaScript Document
+// JavaScript Document
 
-$(document).ready(function (e){
-	document.addEventListener("deviceready",function(){
-		
-		$('#Izquierda').on("swipeleft",function(){
-			navigator.notification. alert("Deslizo a la izquirda",function(){"Aplicacion ","Aceptar"});
-		});
-		
-		$('#derecha').on("swiperight",function(){
-			navigator.notification.confirm("¿Que quieres hacer?",function(opt){
-				switch(opt)
-				{
-					case 1:
-					navigator.notification.beep(1);
-					break;
-					
-					case2:
-					navigator.notification.vibrate(1000);
-					break;
-				}
-				
-			},"Aplicacion ","Beep,Vibrar,Cancelar");
-		});
-	},false);
+$(document).ready(function(e) {
+    document.addEventListener("deviceready", onDeviceReady, false);
+	
 });
+
+     function onDeviceReady(){
+		 
+		 $('#posicion').on('click',function(){
+			 getPosition ();
+		 });
+	 }
+	 
+	  function getPosition(){
+		  var options = {
+			 enableHighAccuracy :true,
+			 maximumAge: 3600000
+	  }
+	  
+	  var watchID = navigator.geolocation.getCurrentPosition(onSucces, onError,options);
+	  
+	  function onSucces(position) {
+		  
+		  alert('Latitude:'     + position.coords.latitude    + '\n' +
+		  'Longitude:'         +position.coords.longitude     +'\n');
+	  };
+	  
+	     function onError(error){
+			 alert('code:'   +error.code  +'\n' + 'message:' + error.message + '\n');
+		 }
+		 }
